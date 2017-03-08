c4a96e289a660c52fc66b7a1af8477c4b0008696
Erinn Bastias <silberio50006@gmail.com>
2017-03-08 13:19:51 +0100

    recipe system finally frickin' done

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,18 +1,59 @@
-	var elements = document.getElementsByClassName('amount');
-	var baseVal = [];
+ var amountOfIngr = document.getElementsByClassName("amount");
+// empty array to store values from HTML later
+ var baseVal = [];
+ var newVal = [];
+// the class "amount" are spans with a number; amountOfIngr is supposed
+//to be each of these amounts
 
-	var portion = document.getElementById('portions').addEventListener("change", checkCurrentPortion);
-	var btn = document.getElementById('btn').addEventListener("click", newFunction);
 
-	function newFunction(){
-		var listValue = document.getElementById('portions').value;
-		//this function prints the value of option in drop down list
-		var numValues = parseInt(document.getElementsByClassName('amount'));
-		document.getElementById('test').innerHTML = numValues;
+//the button which is used in the HTML
+
+ document.getElementById("portions").addEventListener("change", updateIngredients);
+
+ function initBaseValues(){
+
+ 	for (var i = 0; i < amountOfIngr.length; i++) {
+ 	 	var value = parseFloat(amountOfIngr[i].innerHTML);
+	 	baseVal.push(value);
+ 	}
+
+ 	return baseVal;
+ }
+
+
+function selectValue() {
+	//this is the number by which I want to multiply the array elements
+	var portionModifier = parseInt(document.getElementById('portions').value);
+	return portionModifier;
+}
+
+
+function multiplyPortions() {
+	for (var i = 0; i < baseVal.length; i++) {
+		var firstVal = parseFloat(baseVal[i]);
+		var secondVal = parseFloat(selectValue());
+		var sumVal = firstVal * secondVal;
+		var numToString = sumVal.toString();
+		newVal.push(numToString);
 	}
 
-	function checkCurrentPortion() {
-	
-	
-	}
+	return newVal;
+}
 
+function setHTMLValues(newValues) {
+
+		for(var i = 0; i < amountOfIngr.length; i++) {
+		amountOfIngr[i].innerHTML = newValues[i];
+	}
+}
+
+function clearValues() {
+	newVal = [];
+}
+
+function updateIngredients() {
+	initBaseValues();
+	multiplyPortions();
+	setHTMLValues(newVal);
+	clearValues();
+}
